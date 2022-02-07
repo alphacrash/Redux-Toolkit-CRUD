162bd1c2d14776c766721edeca0fea8b03d1a41d
alphacrash <sudhansu.ace@gmail.com>
2022-02-07 23:29:27 +0530

    Redux Toolkit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { configureStore } from "@reduxjs/toolkit";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+
+import App from "./App";
+import usersReducer from "./features/Users";
+import reportWebVitals from "./reportWebVitals";
+
+const store = configureStore({
+  reducer: {
+    users: usersReducer,
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
